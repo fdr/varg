0f7c8f5886f49ea9391376df8d2fe6b196f0ce6c
Daniel Farina <drfarina@acm.org>
2012-04-16 23:49:12 -0700

    Pretty text filling
    Signed-off-by: Daniel Farina <drfarina@acm.org>

--- fill.go
+++ fill.go
@@ -1,6 +1,8 @@
 // To assist with filling paragraphs of text.
 package varg
 
+import "fmt"
+
 type candidate struct {
 	badness   int
 	paragraph []line
@@ -11,22 +13,109 @@ type line struct {
 	width int
 }
 
-func (c *candidate) addWord(w string) {
-	l := &c.paragraph[len(c.paragraph)-1]
-	l.words = append(l.words, w)
-	l.width += len(w)
+func (c *candidate) copy() *candidate {
+	var cpy candidate
+
+	cpy.badness = c.badness
+	cpy.paragraph = make([]line, len(c.paragraph))
+
+	for i, _ := range c.paragraph {
+		l := &cpy.paragraph[i]
+		l.width = c.paragraph[i].width
+		l.words = make([]string, len(c.paragraph[i].words))
+		copy(l.words, c.paragraph[i].words)
+	}
+
+	return &cpy
 }
 
-func (c *candidate) breakLine(targetWidth int) {
+func (c *candidate) fidelity(targetWidth int) {
+	badness := 0
+	delta := 0
+
+	for i := 0; i < len(c.paragraph); i += 1 {
+		l := c.paragraph[i]
+
+		if l.width > targetWidth {
+			if c.badness < 2147483647 {
+				panic(
+					fmt.Sprintf(
+						"Bad badness score: %d, c %v",
+						c.badness, c))
+			}
+
+			badness = 2147483647
+			break
+		}
+
+		delta = targetWidth - l.width
+		badness += delta * delta
+	}
+
+	// Remove the last line if this is a candidate that did not
+	// exceed the maximum line length.
+	if badness != 2147483647 {
+		badness -= delta * delta
+	}
+
+	if badness != c.badness {
+		panic(fmt.Sprintf("Badness score does not total up correctly, "+
+			"got %d expected %d\n%v", c.badness, badness, c))
+	}
+}
+
+func (c *candidate) addWord(targetWidth int, w string) {
+	c.fidelity(targetWidth)
+
+	l := &c.paragraph[len(c.paragraph)-1]
+
+	l.width += len(w)
+
+	if len(l.words) != 0 {
+		// On all words except first in the line, count the
+		// space separation between words against the width of
+		// the line.
+		l.width += 1
+	}
+
+	l.words = append(l.words, w)
+
+	// The last line doesn't count towards badness when it falls
+	// into the target line length, but should it exceed that
+	// length, it is not to be considered an acceptable solution.
+	if l.width > targetWidth {
+		c.badness = 2147483647
+	}
+
+	c.fidelity(targetWidth)
+}
+
+func (c *candidate) breakAndAdd(targetWidth int, w string) {
+	c.fidelity(targetWidth)
+
 	delta := targetWidth - c.paragraph[len(c.paragraph)-1].width
+
 	c.badness += delta * delta
-	c.paragraph = append(c.paragraph, line{})
+	c.paragraph = append(c.paragraph, *newLine())
+
+	c.addWord(targetWidth, w)
+
+	c.fidelity(targetWidth)
+}
+
+func newLine() *line {
+	var l line
+
+	l.words = make([]string, 0)
+
+	return &l
 }
 
 func newCandidate() *candidate {
 	var c candidate
 
 	c.paragraph = make([]line, 1, 30)
+	c.paragraph[0] = *newLine()
 
 	return &c
 }
@@ -40,11 +129,10 @@ func greedyFill(width int, words []string) *candidate {
 	c := newCandidate()
 
 	for _, w := range words {
-		if c.paragraph[len(c.paragraph)-1].width+len(w) > width {
-			c.breakLine(width)
-			c.addWord(w)
+		if c.paragraph[len(c.paragraph)-1].width+len(w) >= width {
+			c.breakAndAdd(width, w)
 		} else {
-			c.addWord(w)
+			c.addWord(width, w)
 		}
 	}
 
@@ -56,24 +144,39 @@ func greedyFill(width int, words []string) *candidate {
 // This searches for a minimal cost as determined by (targetWidth -
 // lineWidth) ** 2.  Candidate solutions have more cost than the
 // greedy fill are eliminated.
-func prettyFill(width int, words []string) *candidate {
+func prettyFill(width int, words []string) (best *candidate) {
 	badnessMax := greedyFill(width, words).badness
 	candidates := make([]candidate, 0, len(words))
 	candidates = append(candidates, *newCandidate())
 
 	for _, w := range words {
-		for _, appendC := range candidates {
+		for i, _ := range candidates {
+			appendC := &candidates[i]
+
 			// Skip if badness has already been exceeded
 			if appendC.badness > badnessMax {
 				continue
 			}
 
-			lineBreakC := appendC
+			lineBreakC := appendC.copy()
+			appendC.addWord(width, w)
+			lineBreakC.breakAndAdd(width, w)
 
-			lineBreakC.breakLine(width)
-			lineBreakC.addWord(w)
+			candidates = append(candidates, *lineBreakC)
 		}
 	}
 
-	return &candidates[0]
+	// Locate minimum badness and return it
+	minBadness := badnessMax
+
+	for i, _ := range candidates {
+		c := &candidates[i]
+
+		if c.badness <= minBadness {
+			best = c
+			minBadness = c.badness
+		}
+	}
+
+	return best
 }
--- fill_test.go
+++ fill_test.go
@@ -1,9 +1,21 @@
 package varg
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
+
+func (c *candidate) render() string {
+	var render []string
+
+	for _, line := range c.paragraph {
+		render = append(render,
+			strings.Join(line.words, " "))
+	}
+
+	return strings.Join(render, "\n")
+}
 
 func TestGreedyFill(t *testing.T) {
 	raw := `read buffer names to search, one by one, ended with RET.  With a prefix argument, they ask for a regexp, and search in buffers whose names match the specified regexp.  Interactively 'multi-isearch-files' and 'multi-isearch-files-regexp' read file names to search, one by one, ended with RET.  With a prefix argument, they ask for a wildcard, and search in file buffers whose file names match the specified wildcard.`
@@ -12,9 +24,123 @@ func TestGreedyFill(t *testing.T) {
 	maxWidth := 72
 	c := greedyFill(maxWidth, words)
 
-	for _, line := range c.paragraph {
-		if line.width > maxWidth {
-			t.FailNow()
+	result := c.render()
+	expected := `read buffer names to search, one by one, ended with RET.  With a prefix
+argument, they ask for a regexp, and search in buffers whose names match
+the specified regexp.  Interactively 'multi-isearch-files' and
+'multi-isearch-files-regexp' read file names to search, one by one,
+ended with RET.  With a prefix argument, they ask for a wildcard, and
+search in file buffers whose file names match the specified wildcard.`
+
+	if result != expected {
+		t.FailNow()
+	}
+}
+
+func TestPrettyFill(t *testing.T) {
+	raw := `read buffer names to search, one by one, ended with RET.  With a prefix argument, they ask for a regexp, and search in buffers whose names match the specified regexp.  Interactively 'multi-isearch-files' and 'multi-isearch-files-regexp' read file names to search, one by one, ended with RET.  With a prefix argument, they ask for a wildcard, and search in file buffers whose file names match the specified wildcard.`
+	words := strings.Split(raw, " ")
+
+	maxWidth := 72
+	c := prettyFill(maxWidth, words)
+	result := c.render()
+	expected := `read buffer names to search, one by one, ended with RET.  With a prefix
+argument, they ask for a regexp, and search in buffers whose names
+match the specified regexp.  Interactively 'multi-isearch-files' and
+'multi-isearch-files-regexp' read file names to search, one by one,
+ended with RET.  With a prefix argument, they ask for a wildcard, and
+search in file buffers whose file names match the specified wildcard.`
+
+	if result != expected {
+		t.FailNow()
+	}
+}
+
+func TestDegenerate(t *testing.T) {
+	raw := `aaaa`
+	words := strings.Split(raw, " ")
+	maxWidth := 8
+
+	p := prettyFill(maxWidth, words)
+	g := greedyFill(maxWidth, words)
+
+	if p.render() != raw {
+		t.FailNow()
+	}
+
+	if g.render() != raw {
+		t.FailNow()
+	}
+}
+
+func TestFillSmall(t *testing.T) {
+	raw := `aaa bb cc ddddd`
+	words := strings.Split(raw, " ")
+
+	maxWidth := 6
+	p := prettyFill(maxWidth, words)
+	g := greedyFill(maxWidth, words)
+
+	pExpected := `aaa
+bb cc
+ddddd`
+
+	gExpected := `aaa bb
+cc
+ddddd`
+
+	if pExpected != p.render() {
+		t.FailNow()
+	}
+
+	if gExpected != g.render() {
+		t.FailNow()
+	}
+}
+
+func newCandidateWithChecker(t *testing.T) (
+	c *candidate, check func(expected string)) {
+	c = newCandidate()
+
+	check = func(expected string) {
+		result := fmt.Sprintf("%v", c)
+
+		if result != expected {
+			t.Fatalf("Expected %s, but got %s", expected, result)
 		}
 	}
+
+	return c, check
+}
+
+func TestAddWord(t *testing.T) {
+	c, check := newCandidateWithChecker(t)
+
+	check("&{0 [{[] 0}]}")
+	c.addWord(4, "a")
+	check("&{0 [{[a] 1}]}")
+	c.addWord(4, "b")
+	check("&{0 [{[a b] 3}]}")
+
+	// Overrun
+	c.addWord(4, "cc")
+	check("&{2147483647 [{[a b cc] 6}]}")
+}
+
+func TestBreakAndAdd(t *testing.T) {
+	c, check := newCandidateWithChecker(t)
+
+	check("&{0 [{[] 0}]}")
+	c.breakAndAdd(4, "a")
+	check("&{16 [{[] 0} {[a] 1}]}")
+	c.breakAndAdd(4, "b")
+	check("&{25 [{[] 0} {[a] 1} {[b] 1}]}")
+	c.breakAndAdd(4, "cc")
+	check("&{34 [{[] 0} {[a] 1} {[b] 1} {[cc] 2}]}")
+	c.breakAndAdd(4, "dddd")
+	check("&{38 [{[] 0} {[a] 1} {[b] 1} {[cc] 2} {[dddd] 4}]}")
+
+	// Overrun
+	c.breakAndAdd(4, "eeeee")
+	check("&{2147483647 [{[] 0} {[a] 1} {[b] 1} {[cc] 2} {[dddd] 4} {[eeeee] 5}]}")
 }
